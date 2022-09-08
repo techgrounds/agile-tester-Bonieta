6e30691f6cb64e649600c89d0b01f1656bd5789d
Bonieta <111249753+Bonieta@users.noreply.github.com>
2022-09-08 14:40:25 +0200

    tutorial

---
+++ Agile-Java/JavaTutorial/src/javaturorial.java
@@ -0,0 +1,35 @@
+public class javaturorial {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+        String firstname = "Bonieta";
+        System.out.println("Your first name is:"+firstname);
+
+    }
+
+/* orinentatie
+        opdracht 1
+
+
+object    object is a member (also called an instance) of a Java class. Each object has an identity, a behavior and a state.
+
+class  are template used to create objects and to define object data types and methods Classes are categories
+
+packaage  A package in Java is used to group related classes. Think of it as a folder in a file directory
+
+constructor A constructor in Java is a special method that is used to initialize objects. The constructor is called when an object of a class is created.
+
+field A field is a class, interface, or enum with an associated value
+
+method  method is a block of code which only runs when it is called. You can pass data, known as parameters, into a method. also known as functions.
+
+pararmeter a variable used to define a particular value during a function definition.
+
+system static void main(string [] args) Java main method is the entry point of any java program afkorting  is p s v m aan elkaar
+
+
+        oefen strings
+*/
+
+}
